c0d649a332863278ec4d13222db12ba93a88382f
tristenwallace <tri3sten@gmail.com>
2020-09-25 19:34:28 -0400

    Implement search functionality

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:title>", views.entry, name="entry")
+    path("<str:title>", views.entry, name="entry"),
+    path("search/", views.search, name="search")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -18,3 +18,28 @@ def entry(request, title):
         "title": title,
         "content": util.get_entry(title)
     })
+
+def search(request):
+    if 'search' in request.GET:
+        search_term = request.GET['search']
+    
+    entries = util.list_entries()
+    results = []
+    
+    for entry in entries:
+        if entry.lower().find(search_term.lower()) != -1:
+            results.append(entry)
+    
+    if not results:
+        return render(request, "encyclopedia/error.html", {
+            "message": "No search results found"
+        })
+    if len(results) is 1:
+        return render(request, "encyclopedia/entry.html", {
+            "title": results[0],
+            "content": util.get_entry(results[0])
+        })
+    
+    return render(request, "encyclopedia/search.html", {
+        "results": results
+    })
